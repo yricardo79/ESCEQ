51fcfa9d99bce562d0bf1ac07d5467196001def6
Johanna <lfarfan21@uan.edu.co>
2020-04-29 01:03:29 -0500

    Se crea lista para los días del mes como prueba.
    Signed-off-by: Johanna <lfarfan21@uan.edu.co>

--- ESCEQ/miscelaneos.py
+++ ESCEQ/miscelaneos.py
@@ -5,4 +5,7 @@ def formatoFecha(request):
     meses = ["Seleccione un mes", "Enero", "Febreo", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
              "Octubre",
              "Noviembre", "Diciembre"]
-    return render(request, 'registroEquino.html', {'meses_del_anio': meses})
+    dias_mes = ["Seleccione día", "1", "2"]
+    return render(request, 'registroEquino.html', {'meses_del_anio': meses, 'dias_mes': dias_mes})
+
+
