d8be0f7e5e859d5409e14a9a2d2e8ac6577a6d58
Johanna <lfarfan21@uan.edu.co>
2020-05-01 23:45:18 -0500

    Se crean en el panel de adminitración la personalización de los campos cambios en los archivos admin.py models.py y el archivo 0005
    Signed-off-by: Johanna <lfarfan21@uan.edu.co>

--- registroEquinos/models.py
+++ registroEquinos/models.py
@@ -13,51 +13,56 @@ class Usuarios(models.Model):
 
 class Raza(models.Model):
     #id_raza = models.CharField(primary_key=True, max_length=100)
-    nom_raza = models.CharField(max_length=255)
-    des_raza = models.CharField(max_length=255)
+    nom_raza = models.CharField(max_length=255, verbose_name="Nombre raza")
+    des_raza = models.CharField(max_length=255, verbose_name="Descripción")
 
     def __str__(self):
         return "La raza es: %s y su descripción es: %s" % (self.nom_raza, self.des_raza)
 
+
 class Comportamiento(models.Model):
     #id_comp = models.CharField(primary_key=True, max_length=100)
-    nom_comp = models.CharField(max_length=255)
-    des_comp = models.CharField(max_length=255)
+    nom_comp = models.CharField(max_length=255, verbose_name="Nombre")
+    des_comp = models.CharField(max_length=255, verbose_name="Descripción")
+
+    def __str__(self):
+        return " : %s : %s" % (self.nom_comp, self.des_comp)
 
 
 class Equino(models.Model):
     #id_chip = models.CharField(primary_key=True, max_length=100)
-    raza_id = models.ForeignKey(Raza, null=True, blank=True, on_delete=models.CASCADE)
-    comp_id = models.ForeignKey(Comportamiento, null=True, blank=True, on_delete=models.CASCADE)
-    nom_equino = models.CharField(max_length=255)
-    fec_nacimiento = models.DateField()
-    adv_manejo = models.CharField(max_length=255)
-    sexo = models.BooleanField()
-    color = models.CharField(max_length=255)
+    raza_id = models.ForeignKey(Raza, null=True, blank=True, on_delete=models.CASCADE, verbose_name="Raza")
+    comp_id = models.ForeignKey(Comportamiento, null=True, blank=True, on_delete=models.CASCADE, verbose_name="Comp")
+    nom_equino = models.CharField(max_length=255, verbose_name="Nombre")
+    fec_nacimiento = models.DateField(verbose_name="Fecha nacimiento")
+    adv_manejo = models.CharField(max_length=255, verbose_name="Adv Manejo")
+    sexo = models.BooleanField(verbose_name="Sexo")
+    color = models.CharField(max_length=255, verbose_name="Color")
 
 
 class Disciplina_Deportiva(models.Model):
     #id_disciplina = models.CharField(primary_key=True, max_length=100)
-    disciplina = models.CharField(max_length=255)
-    descripcion_dis = models.CharField(max_length=255)
+    disciplina = models.CharField(max_length=255, verbose_name="Disciplina")
+    descripcion_dis = models.CharField(max_length=255, verbose_name="Descripción")
 
 
 class Veterinario(models.Model):
     #id_veterinario = models.CharField(primary_key=True, max_length=100)
-    nombres_veterinario = models.CharField(max_length=255)
-    apellidos_veterinario = models.CharField(max_length=255)
-    correo_veterinario = models.CharField(max_length=255)
+    nombres_veterinario = models.CharField(max_length=255, verbose_name="Nombres")
+    apellidos_veterinario = models.CharField(max_length=255, verbose_name="Apellidos")
+    correo_veterinario = models.CharField(max_length=255, verbose_name="Correo veterinario")
 
 
 class Historia_Clinica(models.Model):
     #id_equino_chip = models.CharField(primary_key=True, max_length=100)
-    veterinario_id = models.ForeignKey(Veterinario, null=True, blank=True, on_delete=models.CASCADE)
-    fecha_apertura = models.DateField()
-    estado = models.CharField(max_length=255)
-    observaciones = models.CharField(max_length=255)
+    veterinario_id = models.ForeignKey(Veterinario, null=True, blank=True, on_delete=models.CASCADE, verbose_name="Iden. veterinario")
+    fecha_apertura = models.DateField(verbose_name="Fecha apertura")
+    estado = models.CharField(max_length=255, verbose_name="Estado")
+    observaciones = models.CharField(max_length=255, verbose_name="Observaciones")
 
 
 class Prueba(models.Model):
-    prueba_campo = models.CharField(max_length=255)
+    prueba_campo = models.CharField(max_length=255, verbose_name="Campo de prueba")
+
 """
 """
