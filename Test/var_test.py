8cb5e1ea973c741a848ffdb16413b8253cc1a3f7
Yeimmy <you@example.com>
2021-05-23 15:13:22 -0500

    Comit: Metodos Liberacion e Inicialización

--- Test/var_test.py
+++ Test/var_test.py
@@ -7,27 +7,35 @@ from registro.views import bus_ser_equ2
 https://docs.python.org/3/library/unittest.html#unittest.TestCase.tearDown
 """
 class Test_variables(unittest.TestCase):   
-      
+    def setUp(self):
+        self.resultado=1  
+        self.puntaje1=1   
+        self.puntaje2=1 
+        self.puntaje =1
     def test_bus_ser_equ2(self):
-        resultado = bus_ser_equ2('Magestuoso')
-        self.assertTrue(resultado) 
-
+        self.resultado = bus_ser_equ2('Magestuoso')
+        self.assertTrue(self.resultado) 
     def test_valida_Fallida_ext(self):
-        resultado = validar_extension('jpg')
-        self.assertEqual(resultado,'Incorrecto') 
+        self.resultado = validar_extension('jpg')
+        self.assertEqual(self.resultado,'Incorrecto') 
     def test_valida_ext(self):
-        resultado = validar_extension('.csv')
-        self.assertEqual(resultado,'Correcto') 
+        self.resultado = validar_extension('.csv')
+        self.assertEqual(self.resultado,'Correcto') 
     def test_bus_ser_equ2fallida(self):
-        resultado = bus_ser_equ2('')
-        self.assertFalse(resultado) 
+        self.resultado = bus_ser_equ2('')
+        self.assertFalse(self.resultado) 
     def test_predecir_puntaje(self):
         puntaje = predecir_puntaje(0,0,1,0,0,1,0,0.03,0.03,1,0,1,0,1,0,1,2,32,0,1,1,1,0,20)
         self.assertNotEqual(puntaje,50) 
     def test_predecir_puntajeCalculado(self):
-        puntaje = predecir_puntaje(0,0,1,0,0,1,0,0.03,0.03,1,0,1,0,1,0,1,2,50,0,1,1,1,0,90)
-        self.assertEqual(puntaje,68.92) 
+        self.puntaje = predecir_puntaje(0,0,1,0,0,1,0,0.03,0.03,1,0,1,0,1,0,1,2,50,0,1,1,1,0,90)
+        self.assertEqual(self.puntaje,68.92) 
     def test_predecir_puntajeDos_Variables(self):
-        puntaje1 = predecir_puntaje(0,0,1,0,0,1,0,0.03,0.03,1,0,1,0,1,0,1,2,50,0,1,1,1,0,90)
-        puntaje2 = predecir_puntaje(1,1,1,1,20,50,60,50.12,1.03,1,0,1,0,1,0,1,2,50,0,1,1,1,0,90)
-        self.assertLess(puntaje1,puntaje2)    
+        self.puntaje1 = predecir_puntaje(0,0,1,0,0,1,0,0.03,0.03,1,0,1,0,1,0,1,2,50,0,1,1,1,0,90)
+        self.puntaje2 = predecir_puntaje(1,1,1,1,20,50,60,50.12,1.03,1,0,1,0,1,0,1,2,50,0,1,1,1,0,90)
+        self.assertLess(self.puntaje1,self.puntaje2)    
+    def tearDown(self):
+        self.resultado=0
+        self.puntaje1 =0
+        self.puntaje2=0
+        self.puntaje =0
