0fdcc37589270bd42f28f070b177241749ce5cff
Yeimmy <you@example.com>
2021-05-23 14:50:03 -0500

    Commit: Modificacion de Test

--- Test/var_test.py
+++ Test/var_test.py
@@ -3,25 +3,31 @@ import os
 from ESCEQ.Functions import validar_extension
 from ESCEQ.variables.invocar_modelo import predecir_puntaje
 from registro.views import bus_ser_equ2
-
-class Test_variables(unittest.TestCase):
+"""
+https://docs.python.org/3/library/unittest.html#unittest.TestCase.tearDown
+"""
+class Test_variables(unittest.TestCase):   
+      
     def test_bus_ser_equ2(self):
-        respuesta = bus_ser_equ2('Magestuoso')
-        self.assertTrue(respuesta)   
+        resultado = bus_ser_equ2('Magestuoso')
+        self.assertTrue(resultado) 
 
+    def test_valida_Fallida_ext(self):
+        resultado = validar_extension('jpg')
+        self.assertEqual(resultado,'Incorrecto') 
     def test_valida_ext(self):
-        respuesta = validar_extension('jpg')
-        self.assertEqual(respuesta,'Incorrecto') 
-
+        resultado = validar_extension('.csv')
+        self.assertEqual(resultado,'Correcto') 
     def test_bus_ser_equ2fallida(self):
-        respuesta = bus_ser_equ2('')
-        self.assertFalse(respuesta) 
-
+        resultado = bus_ser_equ2('')
+        self.assertFalse(resultado) 
     def test_predecir_puntaje(self):
         puntaje = predecir_puntaje(0,0,1,0,0,1,0,0.03,0.03,1,0,1,0,1,0,1,2,32,0,1,1,1,0,20)
         self.assertNotEqual(puntaje,50) 
-        
-    def test_predecir_puntajeFallida(self):
+    def test_predecir_puntajeCalculado(self):
         puntaje = predecir_puntaje(0,0,1,0,0,1,0,0.03,0.03,1,0,1,0,1,0,1,2,50,0,1,1,1,0,90)
-        self.assertEqual(puntaje,80) 
- 
+        self.assertEqual(puntaje,68.92) 
+    def test_predecir_puntajeDos_Variables(self):
+        puntaje1 = predecir_puntaje(0,0,1,0,0,1,0,0.03,0.03,1,0,1,0,1,0,1,2,50,0,1,1,1,0,90)
+        puntaje2 = predecir_puntaje(1,1,1,1,20,50,60,50.12,1.03,1,0,1,0,1,0,1,2,50,0,1,1,1,0,90)
+        self.assertLess(puntaje1,puntaje2)    
